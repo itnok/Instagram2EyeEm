fbce87a6324871f08f4e72de0113a7ddc9a4ccec
Simone Conti <s.conti@itnok.com>
2013-01-06 23:43:47 +0100

    New contents and layout for the about page
    Signed-off-by: Simone Conti <s.conti@itnok.com>

---
+++ controllers/about.php
@@ -0,0 +1,5 @@
+<?php
+
+require( 'views/_header.php' );
+require( 'views/about.php' );
+require( 'views/_footer.php' );
---
+++ views/about.php
@@ -0,0 +1,23 @@
+<div class="row-fluid">
+	<div class="span12">
+		<ul class="thumbnails span3">
+			<li>
+				<img src="img/igers2eye.png" alt="Intagram 2 EyeEm">
+			</li>
+		</ul>
+		<div class="hero-unit span9 left">
+			<p>Migrate all your photos from your existing profile on Instagram to your freshly created profile on EyeEm with this free easy 1&#8209;click solution.</p>
+			<p>All you need to use this webapp is:</p>
+			<ul>
+				<li>an account on Instagram with some photos</li>
+				<li>an account on EyeEm where you want to transfer your photo</li>
+				<li>press the button below!</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<a class="btn btn-large btn-success" href="migrate">Migrate NOW!</a>
+	</div>
+</div>
