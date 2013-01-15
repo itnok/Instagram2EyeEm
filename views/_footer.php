7a66d1d028540d511ff7393bb94a07799895dcaa
Simone Conti <s.conti@itnok.com>
2013-01-15 10:54:47 +0100

    [FEATURE] Added Google Analytics track code in the footer
    Signed-off-by: Simone Conti <s.conti@itnok.com>

--- views/_footer.php
+++ views/_footer.php
@@ -53,6 +53,18 @@
 	    	echo "\t" . '<script  type="text/javascript" src="' . $js . '"></script>' . "\n";
     	}
     ?>
+    <script type="text/javascript">
+	    var _gaq = _gaq || [];
+	    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+	    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+	    _gaq.push(['_setAccount', 'UA-XXXXXXX-YY']);
+	    _gaq.push(['_trackPageview']);
+	    (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	    })();
+    </script>
 	<a href="https://github.com/itnok"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub" class="forkme hidden-phone"></a>
 </body>
 </html>
